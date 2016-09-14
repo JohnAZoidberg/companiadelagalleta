d483a0290581a592188ff3f4729a0377b7793ef8
Daniel Schäfer <ds@struckmeierfliesen.de>
2016-09-15 01:37:54 +0200

    Add dbdetails.py without data

---
+++ companiadelagalleta/dbdetails.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python -u
+# coding=utf-8
+
+class dbdetails:
+    # database
+    host = ""
+    user = ""
+    db = ""
+    passwd = ""
+
+    # config
+    server = False
+    serverroot = ""
+    secret = ""
+    email_pw = ""
